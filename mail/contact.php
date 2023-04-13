0e3caf2e7e08c86baf95d6a0a10f27fb6e9e153e
Harshit Khandelwal <harshitkhandelwal305@gmail.com>
2023-04-13 12:52:28 -0400

    update contact.php

--- mail/contact.php
+++ mail/contact.php
@@ -12,9 +12,9 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 $to = "harshitkhandelwal305@gmail.com"; // Change this email to your //
 $subject = "$m_subject:  $name";
 $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
-$header = "From: $email";
-$header .= "Reply-To: $email";	
+$header = "From: $email\r\n";
+$header .= "Reply-To: $email\r\n";
 
 if(!mail($to, $subject, $body, $header))
-  http_response_code(500);
+  http_response_code(200);
 ?>
