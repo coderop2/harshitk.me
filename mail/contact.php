a9c0c7d96ce7470b4c4e8e58344394b0b62976f5
coderop2 <harshitkhandelwal305@gmail.com>
2023-04-13 15:36:50 -0400

    testing

--- mail/contact.php
+++ mail/contact.php
@@ -8,7 +8,8 @@ $name = strip_tags(htmlspecialchars($_POST['name']));
 $email = strip_tags(htmlspecialchars($_POST['email']));
 $m_subject = strip_tags(htmlspecialchars($_POST['subject']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
-
+echo "testing the echo command";
+echo $message;
 $to = "harshitkhandelwal305@gmail.com"; // Change this email to your //
 $subject = "$m_subject:  $name";
 $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
